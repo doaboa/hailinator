d83901b8f951acd6abf0785429a51a9abd954d38
MakerSquare Student <student@challenge-1.att.net>
2013-06-19 14:50:20 -0500

    working on debugging follow and favorite timeout for extensions

--- hailinator.rb
+++ hailinator.rb
@@ -1,10 +1,12 @@
-require "twitter"
 
+
+
+require "twitter"
 Twitter.configure do |config|
-  config.consumer_key = "wdeIymTNup3YNdNdn9ipg"
-  config.consumer_secret = "i8ubBYroljZInX3i3tVPQKg6jQ51fTEylg3qhAzFdw"
-  config.oauth_token = "14168536-UWYdLCEuYkt2TCP3wu70UErI4UmUIalXyIRxb1JQO"
-  config.oauth_token_secret = "eROHt8kc6drCCyrMULosG4dwa1YrXNNtKx7uSUwZAhE"
+  config.consumer_key = "NSY3WhUBhWyNToqypsXExQ"
+  config.consumer_secret = "OsQpfXHUh57khynEIhCyP48sDI3tJ9PFsGpV6a3Q0A"
+  config.oauth_token = "14168536-dXNmUPJt4VKrfk3ovz4QdyUQLLdLwFtF5EbBYtY8"
+  config.oauth_token_secret = "h3n4TbyUMfQBEXrFEJaEiNe0IXCvRtXEyushJnoOSnw"
 end
 
 
@@ -19,6 +21,14 @@ require "csv"
 CSV.open("hailtweets.csv", "wb") do |csv|
   csv << ["handle", "text", "url"]
 	Twitter.search("#hail", :count => 15, :result_type => "recent", ).results.map do |status|
+    Twitter.follow(status.from_user) 
+    sleep 1
+    Twitter.favorite(status.id)
+    sleep 1
 		csv << ["#{status.from_user}", "#{status.text}", "http://www.twitter.com/#{status.from_user}/status/#{status.id}"]
 	end
 end
+
+
+#have twitter automatically favorite all tweets you find
+#have twitter automatically follow all tweeters for the results you found
