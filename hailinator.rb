de99dbf26f8216f9d821b1d5cfd35eb7b08234ad
MakerSquare Student <student@themakersquare.com>
2013-06-20 10:24:53 -0500

    fully working hailinator

--- hailinator.rb
+++ hailinator.rb
@@ -1,7 +1,11 @@
 
 
-
+require "koala"
 require "twitter"
+
+
+@graph = Koala::Facebook::API.new("CAACEdEose0cBAM6TCuhRjcs72KU7P0uK6UYYxNMRQDRX9fDlxLmnbICmzZB7jXKfjlTlNL0N216KZATXOFJEUJBJtsQ5WQZBV79cUDa3irXeqiT61qQRiXQJx65hZAkwkBBWyvSAeleAYRPgqhvf1nOn125hvVwZBTbBLlmbCbAZDZD")
+
 Twitter.configure do |config|
   config.consumer_key = "NSY3WhUBhWyNToqypsXExQ"
   config.consumer_secret = "OsQpfXHUh57khynEIhCyP48sDI3tJ9PFsGpV6a3Q0A"
@@ -20,12 +24,13 @@ require "csv"
 
 CSV.open("hailtweets.csv", "wb") do |csv|
   csv << ["handle", "text", "url"]
-	Twitter.search("#hail", :count => 15, :result_type => "recent", ).results.map do |status|
+	Twitter.search("raptor bosh", :lang => "en", :count => 15, :result_type => "recent", ).results.map do |status|
+    csv << ["#{status.from_user}", "#{status.text}", "http://www.twitter.com/#{status.from_user}/status/#{status.id}"]
     Twitter.follow(status.from_user) 
     sleep 1
     Twitter.favorite(status.id)
     sleep 1
-		csv << ["#{status.from_user}", "#{status.text}", "http://www.twitter.com/#{status.from_user}/status/#{status.id}"]
+		
 	end
 end
 
